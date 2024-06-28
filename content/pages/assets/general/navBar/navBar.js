7029e19712eb75cf4054d0c6c098e11691c1e548
Code Varied <codevaried@gmail.com>
2024-06-28 09:44:14 +0200

    page * -> Ultimas  Update

--- content/pages/assets/general/navBar/navBar.js
+++ content/pages/assets/general/navBar/navBar.js
@@ -3,7 +3,8 @@ import _ from "../js/lib/utilities.js";
 
 //? Manejador de Tokens (Tokens Manager)
 import Tokens from "../js/models/TokensManager.js";
-const T = new Tokens(1000)
+
+const T = new Tokens(3333);
 
 /**
  * @constant {string} navBarPath - Ruta al archivo HTML de la barra de navegación.
